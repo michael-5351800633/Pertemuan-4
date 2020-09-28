bf66bd132b7da478bcc1327853463cd76fd467e8
michael-5351800633 <michael.535180063@stu.untar.ac.id>
2020-09-28 11:32:04 +0700

    Revert "Revert "Initial commit""
    This reverts commit 11b6e7eb39693021187442ba91d427167941dca9.

---
+++ script.js
@@ -0,0 +1,7 @@
+function ChangeLine1Style() {
+    document.getElementById("baris1").className = "makered";
+}
+
+function ChangeStyle(id, classname) {
+    document.getElementById(id).className = classname;
+}
